5a4a376a5305ac1725fb1b06f112e5d29982a099
LAPTOP-3NFP3FHF\leah7 <LeahChater@gmail.com>
2023-01-12 16:21:41 -0600

    Made pokemon info cards display infomation from pokedex.json -Moved hard coded html from a1.html into for loop in script.js -Updated placeholder text for pokemon card data to display actual data for the pokemon in loop -Did not update placeholder text for weaknesses and evolution line yet as they will require multiple if statements -Removed .pokemon-card-class from <div id="output"> in a1.html and moved it into div created in for loop of script.js

--- script.js
+++ script.js
@@ -3,10 +3,55 @@ const output = document.getElementById("output");
 
 
 
-let html = "";
-for (let i = 0; i < data.length; i++) {
-    html += `<p>${data[i].name}: ${data[i].value}</p>`;
+ let html = "";
+for (let i = 0; i < data.pokemon.length; i++) {
+    // html += `<p>${data[i].name}: ${data[i].value}</p>`;
+    html += `
+    <div class="pokemon-card">
+    <h1>${data.pokemon[i].num}</h1>
+    <h1>${data.pokemon[i].name}</h1> 
+    <img src="${data.pokemon[i].img}">
+    <h2>Type:</h2>
+    <ul>
+      <li>${data.pokemon[i].type[0]}</li>
+
+      `
+      //check if pokemon has a second type
+    if (data.pokemon[i].type[1] !== undefined)
+        html += `
+        <li>${data.pokemon[i].type[1]}</li>
+        `
+
+    html += `
+    </ul>
+    <h2>Weaknesses:</h2>
+    <ul>
+      <li>Type 1</li>
+      <li>Type 2</li>
+      <li>Type 3</li>
+    </ul>
+    <p>Height: ${data.pokemon[i].height}</p>
+    <p>Weight: ${data.pokemon[i].weight}</p>
+    <p>Candy :${data.pokemon[i].candy}</p>
+    <p>Candy count: ${data.pokemon[i].candy_count}</p>
+    <p>Egg hatch distance: ${data.pokemon[i].egg}</p>
+    <p>Spawn chance: ${data.pokemon[i].spawn_chance}</p>
+    <p>Average spawn: ${data.pokemon[i].avg_spawns}</p>
+    <p>Spawn time: ${data.pokemon[i].spawn_time}/p>
+    <p>Multipliers: ${data.pokemon[i].multipliers}</p>
+    <h2>Evolution line</h2>
+    <p>Pokemon number:</p>
+    <p>Pokemon Name</p>
+    <p>Pokemon number:</p>
+    <p>Pokemon Name</p>
+    </div>
+    `
+    
 }
 output.innerHTML = html;
 
 
+//TODO aka algorithm :
+//create html that you want to use for how the data will be displayed. Im thinking cards
+//put html into for loop and replace hard coded data with info for each pokemon
+//
