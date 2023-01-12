8550a8ef71d92defa8114c9160124083cbec1fe4
LAPTOP-3NFP3FHF\leah7 <LeahChater@gmail.com>
2023-01-12 14:42:18 -0600

    Linked pokedex.json -Decided to use pokedex.json as my data for the assignment -Changed import line to link to pokedex.json instead of the filler file name that doesnt exist

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-import data from './test.json' assert { type: 'json' };
+import data from './a1-json/pokedex.json' assert { type: 'json' };
 const output = document.getElementById("output");
+
+
 
 let html = "";
 for (let i = 0; i < data.length; i++) {
     html += `<p>${data[i].name}: ${data[i].value}</p>`;
 }
 output.innerHTML = html;
+
+
