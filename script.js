72d0de92eda43ff2a2396d663885e62af0a1502a
LAPTOP-3NFP3FHF\leah7 <LeahChater@gmail.com>
2023-01-16 18:47:06 -0600

    Added some calculated statistics to page -Added heading "Pokemon" to index.html -Added for loop for adding up pokemon statistics to scripts.js -Calculated average spawns for all pokemon and spawn chance for all pokemon in scripts.js -Added html to scripts.js to display statistics on page

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ const output = document.getElementById("output");
 
 
  let html = "";
+ let total_spawns = 0;
+ let total_spawn_chance = 0;
+ for (let i = 0; i < data.pokemon.length; i++) {
+    //adds up avg_spawns for all pokemon
+    total_spawns += data.pokemon[i].avg_spawns;
+    //adds up spawn_chance for all pokemon
+    total_spawn_chance += data.pokemon[i].spawn_chance;
+ }
+//calculates average spawns for all pokemon
+let average_spawns_all = total_spawns / (data.pokemon.length);
+//calculates average spawn chance for all pokemon
+let average_spawn_chance = total_spawn_chance / (data.pokemon.length);
+
+html += `<h2>Statistics:</h2>
+<p><strong>Average spawns all pokemon:</strong> ${average_spawns_all}</p>
+<p><strong>Average spawns chance all pokemon:</strong> ${average_spawn_chance}</p>
+`
+
+
 for (let i = 0; i < data.pokemon.length; i++) {
     let current_pokemon = i;
     // html += `<p>${data[i].name}: ${data[i].value}</p>`;
